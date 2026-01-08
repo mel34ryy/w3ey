6f7b1211027a991d4bb551ad8fe7e5bec6bd86ac
Mohamed El-Ashry <mohomed.1433@hotmail.com>
2026-01-09 00:51:02 +0200

    last commit

--- src/actions/chatbot.js
+++ src/actions/chatbot.js
@@ -3,12 +3,9 @@ import axios from "axios";
 class ChatBotService {
   static async sendMessage(question) {
     try {
-      const response = await axios.post(
-        "https://chatbot-production-d3ac.up.railway.app/chat-bot",
-        {
-          question,
-        }
-      );
+      const response = await axios.post("http://127.0.0.1:5000/chat-bot", {
+        question,
+      });
       return response.data;
     } catch (error) {
       console.error("Error sending message:", error);
